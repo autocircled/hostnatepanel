556bed422dbe3da873101aa0831d2163d3c799a0
Moktadir Rahman LP <autocircled@gmail.com>
2024-07-03 17:05:15 +0600

    all links changed from html to php

--- includes/hooks/addmenutow.php
+++ includes/hooks/addmenutow.php
@@ -6,34 +6,34 @@ add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar) {
     $client = Menu::context('client');
     if (!$client) {
         $primaryNavbar->addChild('Shared Hosting')->setOrder(12);
-        $primaryNavbar->getChild('Shared Hosting')->addChild('Linux Shared Hosting')->setUri('https://hostnate.com/linux-shared-hosting.html')->setOrder(1);
-        $primaryNavbar->getChild('Shared Hosting')->addChild('Windows Shared Hosting')->setUri('https://hostnate.com/windows-shared-hosting.html')->setOrder(2);
-        $primaryNavbar->getChild('Shared Hosting')->addChild('Wordpress Shared Hosting')->setUri('https://hostnate.com/wordpress-shared-hosting.html')->setOrder(3);
+        $primaryNavbar->getChild('Shared Hosting')->addChild('Linux Shared Hosting')->setUri('https://hostnate.com/linux-shared-hosting.php')->setOrder(1);
+        $primaryNavbar->getChild('Shared Hosting')->addChild('Windows Shared Hosting')->setUri('https://hostnate.com/windows-shared-hosting.php')->setOrder(2);
+        $primaryNavbar->getChild('Shared Hosting')->addChild('Wordpress Shared Hosting')->setUri('https://hostnate.com/wordpress-shared-hosting.php')->setOrder(3);
 
         $primaryNavbar->addChild('Reseller Hosting')->setOrder(14);
-        $primaryNavbar->getChild('Reseller Hosting')->addChild('Linux Reseller Hosting')->setUri('https://hostnate.com/linux-reseller-hosting.html')->setOrder(1);
-        $primaryNavbar->getChild('Reseller Hosting')->addChild('Windows Reseller Hosting')->setUri('https://hostnate.com/windows-reseller-hosting.html')->setOrder(2);
+        $primaryNavbar->getChild('Reseller Hosting')->addChild('Linux Reseller Hosting')->setUri('https://hostnate.com/linux-reseller-hosting.php')->setOrder(1);
+        $primaryNavbar->getChild('Reseller Hosting')->addChild('Windows Reseller Hosting')->setUri('https://hostnate.com/windows-reseller-hosting.php')->setOrder(2);
 
 
         $primaryNavbar->addChild('Dedicated Server')->setOrder(15);
-        $primaryNavbar->getChild('Dedicated Server')->addChild('Linux KVM VPS')->setUri('https://hostnate.com/virtualserver-linux-hosting.html')->setOrder(1);
-        $primaryNavbar->getChild('Dedicated Server')->addChild('Dedicated Servers')->setUri('https://hostnate.com/dedicated-server.html')->setOrder(2);
-        $primaryNavbar->getChild('Dedicated Server')->addChild('Windows Dedicated Servers')->setUri('https://hostnate.com/windows-dedicated-server.html')->setOrder(3);
+        $primaryNavbar->getChild('Dedicated Server')->addChild('Linux KVM VPS')->setUri('https://hostnate.com/virtualserver-linux-hosting.php')->setOrder(1);
+        $primaryNavbar->getChild('Dedicated Server')->addChild('Dedicated Servers')->setUri('https://hostnate.com/dedicated-server.php')->setOrder(2);
+        $primaryNavbar->getChild('Dedicated Server')->addChild('Windows Dedicated Servers')->setUri('https://hostnate.com/windows-dedicated-server.php')->setOrder(3);
 
         $primaryNavbar->addChild('Buisness Hosting')->setOrder(13);
-        $primaryNavbar->getChild('Buisness Hosting')->addChild('eCommerce Hosting')->setUri('https://hostnate.com/ecommerce-hosting.html')->setOrder(1);
-        $primaryNavbar->getChild('Buisness Hosting')->addChild('Agency Hosting')->setUri('https://hostnate.com/agency-hosting.html')->setOrder(2);
-        $primaryNavbar->getChild('Buisness Hosting')->addChild('Blog Hosting')->setUri('https://hostnate.com/blog-hosting.html')->setOrder(3);
-        $primaryNavbar->getChild('Buisness Hosting')->addChild('Personal Website Hosting')->setUri('https://hostnate.com/personal-web-hosting.html')->setOrder(4);
-        $primaryNavbar->getChild('Buisness Hosting')->addChild('Nonprofit Web Hosting')->setUri('https://hostnate.com/nonprofit-web-hosting.html')->setOrder(5);
-        $primaryNavbar->getChild('Buisness Hosting')->addChild('LMS Hosting')->setUri('https://hostnate.com/lms-hosting.html')->setOrder(6);
+        $primaryNavbar->getChild('Buisness Hosting')->addChild('eCommerce Hosting')->setUri('https://hostnate.com/ecommerce-hosting.php')->setOrder(1);
+        $primaryNavbar->getChild('Buisness Hosting')->addChild('Agency Hosting')->setUri('https://hostnate.com/agency-hosting.php')->setOrder(2);
+        $primaryNavbar->getChild('Buisness Hosting')->addChild('Blog Hosting')->setUri('https://hostnate.com/blog-hosting.php')->setOrder(3);
+        $primaryNavbar->getChild('Buisness Hosting')->addChild('Personal Website Hosting')->setUri('https://hostnate.com/personal-web-hosting.php')->setOrder(4);
+        $primaryNavbar->getChild('Buisness Hosting')->addChild('Nonprofit Web Hosting')->setUri('https://hostnate.com/nonprofit-web-hosting.php')->setOrder(5);
+        $primaryNavbar->getChild('Buisness Hosting')->addChild('LMS Hosting')->setUri('https://hostnate.com/lms-hosting.php')->setOrder(6);
 
 
         $primaryNavbar->addChild('Domain')->setOrder(11);
-        $primaryNavbar->getChild('Domain')->addChild('Register Domain')->setUri('http://hostnate.com/register-domain.html')->setOrder(1);
+        $primaryNavbar->getChild('Domain')->addChild('Register Domain')->setUri('http://hostnate.com/register-domain.php')->setOrder(1);
         $primaryNavbar->getChild('Domain')->addChild('Renew Domains')->setUri('https://panel.hostnate.com/index.php?rp=/cart/domain/renew')->setOrder(2);
-        $primaryNavbar->getChild('Domain')->addChild('Transfer Domain')->setUri('http://hostnate.com/domain-transfer.html')->setOrder(3);
-        $primaryNavbar->getChild('Domain')->addChild('New Domain Extensions')->setUri('http://hostnate.com/new-domain-extensions.html')->setOrder(4);
+        $primaryNavbar->getChild('Domain')->addChild('Transfer Domain')->setUri('http://hostnate.com/domain-transfer.php')->setOrder(3);
+        $primaryNavbar->getChild('Domain')->addChild('New Domain Extensions')->setUri('http://hostnate.com/new-domain-extensions.php')->setOrder(4);
     } else {
         foreach ($primaryNavbar->getChildren() as $parent_items) {
             if ($parent_items->getName() == "Domains") {
